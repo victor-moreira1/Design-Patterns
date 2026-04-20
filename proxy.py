3ba4f0410f0b49c713cc464c39af33f5baf70afd
Victor Moreira <victormoreira247@gmail.com>
2026-04-20 19:22:14 -0300

    Delete proxy.py

--- proxy.py
+++
@@ -1,8 +0,0 @@
-from time import sleep
-from objeto_real import ObjetoReal
-
-class ProxyLoading:
-    def request_proxy():
-        print("Proxy carregando...")
-        ObjetoReal.request()
-        sleep(10)
